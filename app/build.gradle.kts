48c9cb6a2a112b2ca588a6d15335ea133adc81fc
Ashish <ashishpakhale.official@gmail.com>
2025-08-26 03:25:20 +0530

    Initial Commit - Habit Companion App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.ash.habitcompanion"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ash.habitcompanion"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
